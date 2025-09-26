e2713fb4c12f3bc205c7b5d25c1399ec4d790fab
Victor Eleanya <victoreleanya89@gmail.com>
2025-09-26 18:05:46 +0100

    feat(useLocationSearch.test.tsx): added further test

--- src/__tests__/hooks/useLocationSearch.test.tsx
+++ src/__tests__/hooks/useLocationSearch.test.tsx
@@ -1,30 +1,47 @@
 import { describe, vi, beforeEach, test } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as api from "../../api";
 import { useLocationSearch } from "../../hooks/useLocationSearch";
 import { type ReactNode } from "react";
+import type { SearchResult } from "../../types";
 
-vi.mock("../api", () => ({
+vi.mock("../../api", () => ({
   fetchLocationWeather: vi.fn(),
 }));
 
 const mockFetchLocationWeather = vi.mocked(api.fetchLocationWeather);
 
-const mockSearchResults = [
+const mockSearchResults: SearchResult[] = [
   {
-    id: 1,
-    name: "New York",
-    country: "US",
+    admin1: "New York",
+    admin1_id: 5128638,
+    country: "United States",
+    country_code: "US",
+    country_id: 6252001,
+    elevation: 10,
+    feature_code: "PPL",
+    id: 5128581,
     latitude: 40.7128,
     longitude: -74.006,
+    name: "New York",
+    population: 8175133,
+    timezone: "America/New_York",
   },
   {
-    id: 2,
-    name: "New York Mills",
-    country: "US",
+    admin1: "Minnesota",
+    admin1_id: 5037779,
+    country: "United States",
+    country_code: "US",
+    country_id: 6252001,
+    elevation: 408,
+    feature_code: "PPL",
+    id: 5043473,
     latitude: 46.518,
     longitude: -95.3731,
+    name: "New York Mills",
+    population: 1199,
+    timezone: "America/Chicago",
   },
 ];
 
@@ -82,5 +99,24 @@ describe("useLocationSearch", () => {
     await new Promise((resolve) => setTimeout(resolve, 100)); // wait for any async operations
 
     expect(result.current.fetchStatus).toBe("idle");
+  });
+
+  test("should fetch and return results when debouncedValue is provided", async () => {
+    mockFetchLocationWeather.mockResolvedValueOnce(mockApiResponse);
+
+    const { result } = renderHook(() => useLocationSearch("New York"), {
+      wrapper: createWrapper(queryClient),
+    });
+
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.isEnabled).toBe(true);
+
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+    });
+
+    expect(result.current.data).toEqual(mockSearchResults);
+    expect(mockFetchLocationWeather).toHaveBeenCalledWith("New York");
+    expect(mockFetchLocationWeather).toHaveBeenCalledTimes(1);
   });
 });
