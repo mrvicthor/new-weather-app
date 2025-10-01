f7bc83de24a2a7ed37dd5f4b9f8a5b27107d62d9
Victor Eleanya <victoreleanya89@gmail.com>
2025-10-01 10:21:42 +0100

    feat(searchResults.test.tsx): should not render list when searchResults is undefined or empty

---
+++ src/__tests__/components/searchresults.test.tsx
@@ -0,0 +1,104 @@
+import { vi } from "vitest";
+import { useLocationStore } from "../../hooks/useLocationStore";
+import type { SearchResult } from "../../types";
+import type { LocationStore } from "../../store/location.store";
+import { render } from "@testing-library/react";
+import SearchResults from "../../components/searchResult";
+
+vi.mock("../../hooks/useLocationStore");
+
+const mockUseLocationStore = vi.mocked(useLocationStore);
+
+describe("SearchResults Component", () => {
+  let isLoading = false;
+  let debouncedValue = "";
+  const mockSearchResult: SearchResult[] = [
+    {
+      admin1: "Illinois",
+      admin1_id: 1,
+      country: "United States",
+      country_code: "US",
+      country_id: 840,
+      elevation: 182,
+      feature_code: "PPLA",
+      id: 2001,
+      latitude: 39.7817,
+      longitude: -89.6501,
+      name: "Springfield",
+      population: 114000,
+      timezone: "America/Chicago",
+    },
+    {
+      admin1: "Massachusetts",
+      admin1_id: 2,
+      country: "United States",
+      country_code: "US",
+      country_id: 840,
+      elevation: 21,
+      feature_code: "PPLA2",
+      id: 2002,
+      latitude: 42.1015,
+      longitude: -72.5898,
+      name: "Springfield",
+      population: 155000,
+      timezone: "America/New_York",
+    },
+    {
+      admin1: "Missouri",
+      admin1_id: 3,
+      country: "United States",
+      country_code: "US",
+      country_id: 840,
+      elevation: 396,
+      feature_code: "PPLA2",
+      id: 2003,
+      latitude: 37.2089,
+      longitude: -93.2923,
+      name: "Springfield",
+      population: 170000,
+      timezone: "America/Chicago",
+    },
+  ];
+
+  const mockSetLocation = vi.fn();
+  const mockSetSearchQuery = vi.fn();
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+    isLoading = false;
+    debouncedValue = "";
+    mockUseLocationStore.mockImplementation((selector) => {
+      return selector({
+        setLocation: mockSetLocation,
+        setSearchQuery: mockSetSearchQuery,
+      } as unknown as LocationStore);
+    });
+  });
+
+  test("should not render list when debouncedValue is empty", () => {
+    render(
+      <SearchResults
+        isLoading={isLoading}
+        searchResults={mockSearchResult}
+        debouncedValue={debouncedValue}
+      />
+    );
+
+    const list = document.querySelector("ul");
+    expect(list).not.toBeInTheDocument();
+  });
+
+  test("should not render list when searchResults is undefined or empty", () => {
+    const searchResults: SearchResult[] = [];
+    debouncedValue = "test city";
+    render(
+      <SearchResults
+        isLoading={isLoading}
+        searchResults={searchResults}
+        debouncedValue={debouncedValue}
+      />
+    );
+    const list = document.querySelector("ul");
+    expect(list).not.toBeInTheDocument();
+  });
+});
